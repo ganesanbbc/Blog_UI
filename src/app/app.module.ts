b860f8b1ef2633861dc11a262b423ecdedc366ad
GanesanNS <ganesan@arivuventures.com>
2017-12-17 15:57:08 +0530

    Header content implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule, MatCardModule, MatMenuModule, MatToolbarModule, MatIconModule } from '@angular/material';
 
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { ContentComponent } from './components/content/content.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ContentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,  
+    BrowserAnimationsModule,
+    HttpModule,HttpClientModule,
+    MatButtonModule, 
+    MatCheckboxModule, 
+    MatCardModule, 
+    MatMenuModule,   
+    MatToolbarModule, 
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
