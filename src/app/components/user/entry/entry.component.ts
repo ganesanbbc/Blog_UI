493858134ba9c860be540e78dc8ea69ec32b1c27
GanesanNS <ganesan@arivuventures.com>
2018-01-01 11:08:45 +0530

    Add user

--- src/app/components/user/entry/entry.component.ts
+++ src/app/components/user/entry/entry.component.ts
@@ -1,4 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import {Location} from '@angular/common';
+import {
+  ReactiveFormsModule,
+  FormsModule,
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder
+} from '@angular/forms';
+import { NgbDateParserFormatter, NgbDateStruct } from "@ng-bootstrap/ng-bootstrap";
+
+import { User } from "../user.model"
+import { UserentryService } from "./userentry.service"
+
 
 @Component({
   selector: 'app-entry',
@@ -7,9 +21,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserEntryComponent implements OnInit {
 
-  constructor() { }
+
+  form: FormGroup;
+  _location: Location;
+
+  @Input() user:User;
+
+  constructor(private formBuilder: FormBuilder, 
+    private _service: UserentryService,
+    private location: Location) {
+      this._location = location;
+       
+     }
 
   ngOnInit() {
+    this.form = this.formBuilder.group({
+      name: [null],
+      email: [null],
+      role: [null]
+    });
+
+    this.user = new User();
   }
+  
+
+  onSubmit() {
+    if (this.form.valid) {
+     
+     this.user.name = this.form.get('name').value;
+     this.user.role = this.form.get('role').value;
+     this.user.email = this.form.get('email').value;
+
+      this._service.createUser(this.user);
+      this._location.back();
+
+    } else {
+      // validate all form fields
+    }
+  }
+  
 
 }
--- src/app/components/user/entry/userentry.service.ts
+++ src/app/components/user/entry/userentry.service.ts
@@ -1,8 +1,33 @@
 import { Injectable } from '@angular/core';
+import {HttpModule, Http,Response} from '@angular/http';
+import 'rxjs/add/operator/map'
+
+import { User } from "../user.model"
 
 @Injectable()
 export class UserentryService {
 
-  constructor() { }
+  addUser: string = 'http://localhost:8080/users';
+  http: Http;
+
+  constructor(public _http: Http) {
+    this.http = _http;
+   }
+
+   createUser(user:User){
+
+    console.log('In Service::::::: '+ user.name);
+    
+    this.http.post(this.addUser, user, { 
+    }).subscribe(
+      res => {
+        console.log(res);
+      },
+      err => {
+        console.log("Error occured");
+      }
+    );
+  
+  }
 
 }
